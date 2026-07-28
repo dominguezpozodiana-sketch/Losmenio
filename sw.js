be04ba32731e58f4ef3a0f83a338c24bff0f9f94
dominguezpozodiana-sketch <dominguezpozodiana@gmail.com>
2026-07-28 05:08:36 -0400

    jdhhdh

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
 const CACHE_NAME = 'mi-lista-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
+// Instalación
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -13,17 +14,22 @@ self.addEventListener('install', event => {
   );
 });
 
+// Activación
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
       );
     }).then(() => self.clients.claim())
   );
 });
 
+// Fetch (cache-first)
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
